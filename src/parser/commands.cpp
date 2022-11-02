c139f5bb9450d24823b8bb21d94e8b6d166f3ccf
redoakleaves <soenke.schollmeyer@gmail.com>
2022-11-02 14:43:44 +0100

    Add documentation for command parsing

--- src/parser/commands.cpp
+++ src/parser/commands.cpp
@@ -1,4 +1,6 @@
+#include <sstream>
 #include <regex>
+#include <algorithm>
 
 #include "tools/color.h"
 #include "commands.h"
@@ -8,19 +10,20 @@ static const std::regex command_expression("^:([a-zA-Z]+)");
 int parse_commands(Entry& entry, std::string& substring) {
     std::smatch match;
     if (std::regex_match(substring, match, command_expression)) {
+        // Convert command to lowercase
         std::string command = match[1].str();
         std::transform(command.begin(), command.end(), command.begin(), [](unsigned char c) {
             return std::tolower(c);
         });
 
+        // Colorize command in entry
         std::stringstream stream;
-        stream << '{';
-        stream << COLOR_COMMAND;
-        stream << '}';
+        stream << '{' << COLOR_COMMAND << '}';
         stream << match[0].str();
         std::string stylized = stream.str();
         entry.set_stylized(stylized);
 
+        // Return int representing command
         if (command.compare("exit") == 0)
             return Commands::Exit;
     }
--- src/parser/commands.h
+++ src/parser/commands.h
@@ -1,12 +1,22 @@
 #ifndef _COMMAND_H_
 #define _COMMAND_H_
 
+#include <string>
+
 #include "entry.h"
 
 enum Commands {
     Exit = 1
 };
 
+/**
+ * @brief Searches for command usage within a substring.
+ * 
+ * @param entry Reference to the current entry.
+ * @param substring Reference to the substring.
+ * 
+ * @return Returns 0 if no command is found, otherwise an integer representing the command is returned.
+*/
 int parse_commands(Entry& entry, std::string& substring);
 
 #endif
