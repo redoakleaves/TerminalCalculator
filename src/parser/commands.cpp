ccae6fb131a2b4c430a7cbe7b0a03ead2419bc02
redoakleaves <soenke.schollmeyer@gmail.com>
2022-11-03 16:53:10 +0100

    Add re2 library

--- src/parser/commands.cpp
+++ src/parser/commands.cpp
@@ -1,17 +1,19 @@
 #include <sstream>
-#include <regex>
+#include <string>
 #include <algorithm>
+
+#include <re2/re2.h>
 
 #include "tools/color.h"
 #include "commands.h"
 
-static const std::regex command_expression("^:([a-zA-Z]+)");
+static const re2::RE2 command_expression("^:([a-zA-Z]+)");
 
 int parse_commands(Entry& entry, std::string& substring) {
-    std::smatch match;
-    if (std::regex_match(substring, match, command_expression)) {
+    re2::StringPiece match;
+    if (re2::RE2::FullMatch(substring, command_expression, &match)) {
         // Convert command to lowercase
-        std::string command = match[1].str();
+        std::string command = match.ToString();
         std::transform(command.begin(), command.end(), command.begin(), [](unsigned char c) {
             return std::tolower(c);
         });
@@ -19,12 +21,12 @@ int parse_commands(Entry& entry, std::string& substring) {
         // Colorize command in entry
         std::stringstream stream;
         stream << '{' << COLOR_COMMAND << '}';
-        stream << match[0].str();
+        stream << ':' << match.ToString();
         std::string stylized = stream.str();
         entry.set_stylized(stylized);
 
         // Return int representing command
-        if (command.compare("exit") == 0)
+        if (command == "exit")
             return Commands::Exit;
     }
 
